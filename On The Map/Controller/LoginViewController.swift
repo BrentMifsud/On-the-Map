0edb6a0f481053bcfd517fa018de503797ffbc1b
Brent Mifsud <brentmifsud@icloud.com>
2019-06-02 15:32:59 -0400

    Setup basic login screen layout

---
+++ On The Map/Controller/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  On The Map
+//
+//  Created by Brent Mifsud on 2019-06-02.
+//  Copyright © 2019 Brent Mifsud. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
