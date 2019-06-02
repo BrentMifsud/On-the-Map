b16f73bd5d067b1bc59d18cafd04c8483af6e6e7
Brent Mifsud <brentmifsud@icloud.com>
2019-06-02 16:48:18 -0400

    add map and list view controllers

---
+++ On The Map/Controller/MapViewController.swift
@@ -0,0 +1,61 @@
+//
+//  MapViewController.swift
+//  On The Map
+//
+//  Created by Brent Mifsud on 2019-06-02.
+//  Copyright © 2019 Brent Mifsud. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension MapViewController: MKMapViewDelegate {
+	func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+
+		let reuseId = "pin"
+
+		var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+
+		if pinView == nil {
+			pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+			pinView?.canShowCallout = true
+			pinView?.pinTintColor = .red
+			pinView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+		} else {
+			pinView!.annotation = annotation
+		}
+
+		return pinView
+	}
+
+	func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+		if control == view.rightCalloutAccessoryView {
+			let app = UIApplication.shared
+			if let toOpen = view.annotation?.subtitle! {
+				app.open(URL(string: toOpen)!, options: [:], completionHandler: nil)
+			}
+		}
+	}
+
+}
